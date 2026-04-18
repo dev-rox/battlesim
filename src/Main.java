c6659be4a33bd7a2f1c6c69fa0d4e0e82b3693f3
Raul <test@test.com>
2026-04-18 17:30:14 -0300

    Atualizado Main para simular uma batalha.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    static void main() {
-        System.out.println("hello world");
+    public static void main(String[] args) {
+        Guerreiro guerreiro1 = new Guerreiro("Fulano", 100, 20, 10);
+        Guerreiro guerreiro2 = new Guerreiro("Ciclano", 90, 18, 8);
+
+        guerreiro1.exibirStatus();
+        System.out.println();
+
+        guerreiro1.atacar(guerreiro2);
+        guerreiro2.usarHabilidadeEspecial(guerreiro1);
+
+        System.out.println();
+        guerreiro1.exibirStatus();
+        System.out.println();
+        guerreiro2.exibirStatus();
     }
 }
