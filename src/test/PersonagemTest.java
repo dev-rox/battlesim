d1cdd8bd65133c2af334b0f5d7ebef65b69a17c2
Raul <test@test.com>
2026-04-26 14:51:11 -0300

    Adiciona testes unitarios com JUnit e GitHub Actions

---
+++ src/test/PersonagemTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonagemTest {
+
+    @Test
+    void guerreiroDeveAtacarMagoEReduzirHp() {
+        Guerreiro guerreiro = new Guerreiro("Arthur", 120, 20, 8);
+        Mago mago = new Mago("Merlin", 80, 18, 5, 30);
+
+        guerreiro.atacar(mago);
+
+        assertEquals(65, mago.getHp());
+    }
+
+    @Test
+    void magoDeveUsarBolaDeFogoConsumindoManaECausandoDano() {
+        Mago mago = new Mago("Merlin", 80, 18, 5, 30);
+        Guerreiro guerreiro = new Guerreiro("Arthur", 120, 20, 8);
+
+        mago.usarHabilidadeEspecial(guerreiro);
+
+        assertEquals(95, guerreiro.getHp());
+        assertEquals(20, mago.getMana());
+    }
+
+    @Test
+    void guerreiroDeveUsarGolpePesadoECausarDanoEspecial() {
+        Guerreiro guerreiro = new Guerreiro("Arthur", 120, 20, 8);
+        Mago mago = new Mago("Merlin", 80, 18, 5, 30);
+
+        guerreiro.usarHabilidadeEspecial(mago);
+
+        assertEquals(45, mago.getHp());
+    }
+}
